a72f34664d460240aea962a81c9169637b2fe1a3
nate <41275256+nateci@users.noreply.github.com>
2024-12-30 16:10:24 -0600

    Create main.py

---
+++ module/main.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+from tkinter import messagebox
+from entropy_calculator import EntropyCalculator
+from password_analyzer import PasswordAnalyzer
+from password_generator import PasswordGenerator
